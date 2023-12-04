ada8cc360fd1f6d3f00de65ec9dfe8cbf60f82c2
spopoola1 <146796212+spopoola1@users.noreply.github.com>
2023-12-03 21:55:07 -0500

    Add files via upload

--- src/Pages/NewSimulation.js
+++ src/Pages/NewSimulation.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import NewSimulationForm from '../components/Simulations/SimulationForm';
-import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
+import { useNavigate } from 'react-router-dom';
 
+//function for creating a new simulation
 function NewSimulationPage() {
   const navigate = useNavigate();
 
+
+  //function for handling form submissions
   const submitHandler = (event) => {
     event.preventDefault();
 
-    // Perform your form validations here
+
     // If validations are successful, navigate to the Simulation Details page
     navigate('/simulation-details');
   };
